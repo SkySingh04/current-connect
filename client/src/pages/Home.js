269af09d3225a645f97c56c4a45bc220612768e4
Shirish Kumar <shirishk2003@gmail.com>
2023-08-29 19:39:06 +0530

    asd

---
+++ client/src/pages/Home.js
@@ -0,0 +1,42 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+
+export const Home = () => {
+  const navigate = useNavigate();
+  const StickyNav = () => {
+    useEffect(() => {
+      const handlescroll = () => {
+        const nav = document.querySelector("nav");
+        if (document.documentElement.scrollTop > 20) {
+          nav.classList.add("sticky");
+        } else {
+          nav.classList.remove("sticky");
+        }
+      };
+      window.addEventListener("scroll", handlescroll);
+      return () => {
+        window.removeEventListener("scroll", handlescroll);
+      };
+    }, []);
+
+    return <nav>navigation</nav>;
+  };
+  return (
+    <div className="">
+      <div className="home">
+        <section className="mainsec">
+          <h1>
+          "Current Solutions for Your Electric Bills: Fast, Secure, and User-Friendly."
+          </h1>
+          <button onClick={() => navigate("/login")} className="loginbtn">
+            LOGIN
+          </button>
+
+        </section>
+      </div>
+
+
+    </div>
+  );
+};
